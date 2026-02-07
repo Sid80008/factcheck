f0f2e79be6b685d2e82ebf2885caf520c9fcbd12
SIDDHARTH UMAJWAL <160970705+Sid80008@users.noreply.github.com>
2026-02-08 02:15:45 +0530

    Create MediaBiasCard.jsx

---
+++ src/components/MediaBiasCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/App.css";
+
+const MediaBiasCard = ({ mediaBias, narrativeTags }) => {
+  const getBiasColor = (bias) => {
+    switch (bias.toLowerCase()) {
+      case "left":
+        return "#ff4d4d";
+      case "right":
+        return "#4d79ff";
+      case "neutral":
+        return "#0ff";
+      default:
+        return "#fff";
+    }
+  };
+
+  return (
+    <div className="card media-bias-card">
+      <h3 style={{ color: getBiasColor(mediaBias) }}>Media Bias: {mediaBias}</h3>
+      <div className="narrative-tags">
+        {narrativeTags.map((tag, index) => (
+          <span key={index} className="tag">
+            {tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MediaBiasCard;
