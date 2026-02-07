ec77c1fdcddfe262c5a0fe93d4cf8334a58d154f
SIDDHARTH UMAJWAL <160970705+Sid80008@users.noreply.github.com>
2026-02-08 02:18:17 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      🔮 Welcome to Weird World — where truth and fiction shake hands in the dark.
+    </footer>
+  );
+};
+
+export default Footer;
