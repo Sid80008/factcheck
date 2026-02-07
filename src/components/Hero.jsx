92fe032a1ce506072f342cb894aa0df960db4ee0
SIDDHARTH UMAJWAL <160970705+Sid80008@users.noreply.github.com>
2026-02-08 02:11:21 +0530

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/App.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="hero-title glow">News Verification 2050</h1>
+      <p className="hero-subtitle glow">
+        Instantly check authenticity, trust score, and sources of any news
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
